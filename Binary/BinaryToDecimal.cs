c3cf046f07f392e32a03cd051b6cb47434a96d63
Tofiasen <roenberg04@gmail.com>
2025-05-19 08:39:45 +0200

    Final
    Fixed Aston Not Uploading The correct Code

--- Binary/BinaryToDecimal.cs
+++ Binary/BinaryToDecimal.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +12,57 @@ namespace BinaryConverter
         public static void Run()
         {
             Console.WriteLine("Binary to Decimal Converter");
-            ConvertBinaryToDecimal("01111111.00000000.00000000.00000001");
+            Console.Write("Indtast et binært tal: ");
+            string binaryInput = Console.ReadLine();
+
+            if (!IsBinary(binaryInput))
+            {
+                Console.WriteLine("Ugyldigt input! Brug kun 0 og 1.");
+                return;
+            }
+
+            Console.WriteLine(ConvertBinaryToDecimal(binaryInput));
         }
 
-        public static string ConvertBinaryToDecimal(string binary)
+        public static int ConvertBinaryToDecimal(string binaryString)
         {
-            Console.WriteLine($"Example: {binary} => 127.0.0.1");
-            return "127.0.0.1";
+            
+            List<int> powerOfTwos = new List<int>();
+            int value = 1;
+
+            for (int i = 0; i < binaryString.Length; i++)
+            {
+                powerOfTwos.Insert(0, value);
+                value *= 2;
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < binaryString.Length; i++)
+            {
+                Console.WriteLine("i" + i);
+                int number = binaryString[i] - '0';
+                Console.WriteLine(number);
+
+                int powerOfTwo = powerOfTwos[i];
+                Console.WriteLine(powerOfTwo);
+
+                sum += number * powerOfTwo;
+            }
+
+            Console.WriteLine($"Eksempel: {binaryString} => {sum}");
+
+            return sum;
+        }
+
+        public static bool IsBinary(string input)
+        {
+            foreach (char c in input)
+            {
+                if (c != '0' && c != '1')
+                    return false;
+            }
+            return true;
         }
     }
 }
